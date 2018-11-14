59b27bea0ba5ebcda111a8a9338f24a6505af288
Alex <tucanae@icloud.com>
2018-11-14 16:30:35 +0000

    Initial Commit

---
+++ NSNetworkingMocker/NSNetworkingMocker.h
@@ -0,0 +1,19 @@
+//
+//  NSNetworkingMocker.h
+//  NSNetworkingMocker
+//
+//  Created by Alex on 14/11/2018.
+//  Copyright © 2018 Netsells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSNetworkingMocker.
+FOUNDATION_EXPORT double NSNetworkingMockerVersionNumber;
+
+//! Project version string for NSNetworkingMocker.
+FOUNDATION_EXPORT const unsigned char NSNetworkingMockerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSNetworkingMocker/PublicHeader.h>
+
+
